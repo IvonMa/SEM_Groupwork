595b35e4bb7f239244b725a3bb24dd374107be1c
JohnMercer32 <61018690+JohnMercer32@users.noreply.github.com>
2020-02-18 16:12:46 +0000

    Buffers and Destroy scripts
    Added DestroyByContact and DestroyByTime scripts to the default asteroid.
    On spawn the scale of the asteroid changes, with a buffer applied to both scale size and spawn position from the previous asteroid

--- Assets/GameController.cs
+++ Assets/GameController.cs
@@ -5,10 +5,23 @@ using UnityEngine;
 public class GameController : MonoBehaviour
 {
     public GameObject hazard;
-    public float xVal = 30.0f;              //set this to just beyond screen
-    public float yMin = -10.0f;
-    public float yMax = 10.0f;
+
+    //TODO: fit these to screen size
+    public float xVal = 30.0f;
+    public float yVal = 0.0f;
+    public float yMin = -3.0f;
+    public float yMax = 3.0f;
+    public float prevSpawn = 1.0f;
+    public float spawnBuffer = 0.5f;
+
     public float spawnWait = 5.0f;
+
+    public float randScale = 1.0f;
+    public float prevScale = 1.0f;
+   
+    public float scaleBuffer = 0.1f;
+    public float scaleMin = 0.25f;
+    public float scaleMax = 0.75f;
 
     public float hazardSpeed = -2.0f;
 
@@ -22,15 +35,21 @@ public class GameController : MonoBehaviour
     {
         while (true)
         {
+            while (Mathf.Abs(prevSpawn - yVal) < spawnBuffer) yVal = Random.Range(yMin, yMax);
             Vector2 spawnPosition = new Vector2(
                 xVal,
-                Random.Range(yMin, yMax));
+                yVal);
+            prevSpawn = yVal;
 
             GameObject asteroid = Instantiate(hazard, spawnPosition, Quaternion.identity);
 
+            while (Mathf.Abs(prevScale - randScale) < scaleBuffer) randScale = Random.Range(scaleMin, scaleMax);
+            asteroid.transform.localScale = new Vector3(randScale, randScale, randScale);
+            prevScale = randScale;
+
             asteroid.GetComponent<Rigidbody2D>().velocity = new Vector2(hazardSpeed, 0);
 
-            if (spawnWait > 1.0f) spawnWait -= 0.05f;
+            if (spawnWait > 1.0f) spawnWait -= 0.01f;
 
             yield return new WaitForSeconds(spawnWait);
         }
