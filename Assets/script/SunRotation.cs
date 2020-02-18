463e23572a4cad9f1b83e893f21e3326f3d73ee6
MA Yunfeng <ivonma0222@gmail.com>
2020-02-18 20:11:01 +0000

    Sun rotation
    The sun can rotate in the same direction with the background.

---
+++ Assets/script/SunRotation.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SunRotation : MonoBehaviour
+{
+    public float rotationSpeed = 1f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(transform.position, Vector3.forward, rotationSpeed);
+    }
+}
