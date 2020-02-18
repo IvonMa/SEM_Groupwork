45835d37960e27557e8cee849d13483bf72ca415
MA Yunfeng <ivonma0222@gmail.com>
2020-02-18 17:33:44 +0000

    Pull player back to origin position after collision
    After collision, there will be a velocity which pull the player back to original position.

--- Assets/script/PlayerHandler.cs
+++ Assets/script/PlayerHandler.cs
@@ -5,7 +5,8 @@ using UnityEngine;
 public class PlayerHandler : MonoBehaviour
 {
     private const float JUMP_AMOUNT = 5f;
-    private readonly Vector3 ORIGINAL_PLAYER_POSITION = new Vector3(-2f, 0, 0);
+    private const float ORIGINAL_PLAYER_POSITION_X = -2f;
+    private readonly Vector3 ORIGINAL_PLAYER_POSITION = new Vector3(ORIGINAL_PLAYER_POSITION_X, 0, 0);
 
     private Rigidbody2D playerRigidbody;
 
@@ -21,6 +22,11 @@ public class PlayerHandler : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             playerRigidbody.velocity = Vector2.up * JUMP_AMOUNT;
+        }
+
+        if (transform.position.x < ORIGINAL_PLAYER_POSITION_X)
+        {
+            playerRigidbody.velocity = Vector2.right;
         }
     }
 }
