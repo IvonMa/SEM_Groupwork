37106202a457781967ce79292b17beefabc5f0fc
JohnMercer32 <61018690+JohnMercer32@users.noreply.github.com>
2020-02-20 04:18:57 +0000

    Playing boolean
    Inserted a playing boolean to the game controller and player handler.
    An auto jump coroutine is added to the player handler for the pre-game state

---
+++ Assets/script/GameController.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    public GameObject hazard;
+
+    public bool playing = false;
+
+    //TODO: fit these to screen size
+    public float xVal = 15.0f;
+    public float yVal = 0.0f;
+    public float yMin = -3.0f;
+    public float yMax = 4.0f;
+
+    public float prevSpawn = 1.0f;
+    public float spawnBuffer = 0.5f;
+
+    public float spawnWait = 5.0f;
+
+    public float randScale = 1.0f;
+    public float prevScale = 1.0f;
+
+    public float scaleBuffer = 0.1f;
+    public float scaleMin = 0.25f;
+    public float scaleMax = 0.75f;
+
+    public float hazardSpeed = -2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(SpawnAsteroids());
+    }
+
+    IEnumerator SpawnAsteroids()
+    {
+        while (true)
+        {
+            if (playing)
+            {
+                while (Mathf.Abs(prevSpawn - yVal) < spawnBuffer) yVal = Random.Range(yMin, yMax);
+                Vector2 spawnPosition = new Vector2(
+                    xVal,
+                    yVal);
+                prevSpawn = yVal;
+
+                GameObject asteroid = Instantiate(hazard, spawnPosition, Quaternion.identity);
+
+                while (Mathf.Abs(prevScale - randScale) < scaleBuffer) randScale = Random.Range(scaleMin, scaleMax);
+                asteroid.transform.localScale = new Vector3(randScale, randScale, randScale);
+                prevScale = randScale;
+
+                asteroid.GetComponent<Rigidbody2D>().velocity = new Vector2(hazardSpeed, 0);
+
+                if (spawnWait > 1.0f) spawnWait -= 0.01f;
+
+                yield return new WaitForSeconds(spawnWait);
+            }
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!playing)
+        {
+            // User control 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playing = true;
+            }
+        }
+        
+    }
+}
--- Assets/script/PlayerHandler.cs
+++ Assets/script/PlayerHandler.cs
@@ -10,23 +10,53 @@ public class PlayerHandler : MonoBehaviour
 
     private Rigidbody2D playerRigidbody;
 
+    private bool playing = false;
+
+    private float jumpWait = 1.0f;
+
     void Start()
     {
         playerRigidbody = GetComponent<Rigidbody2D>();
         transform.position = ORIGINAL_PLAYER_POSITION;
+        StartCoroutine(AutoJump());
+    }
+
+    IEnumerator AutoJump()
+    {
+        while (true)
+        {
+            if (!playing)
+            {
+                playerRigidbody.velocity = Vector2.up * JUMP_AMOUNT;
+
+                yield return new WaitForSeconds(jumpWait);
+            }
+        }
     }
 
     void Update()
     {
-        // User control 
-        if (Input.GetKeyDown(KeyCode.Space))
+       if (playing)
         {
-            playerRigidbody.velocity = Vector2.up * JUMP_AMOUNT;
-        }
+            // User control 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playerRigidbody.velocity = Vector2.up * JUMP_AMOUNT;
+            }
 
-        if (transform.position.x < ORIGINAL_PLAYER_POSITION_X)
+            if (transform.position.x < ORIGINAL_PLAYER_POSITION_X)
+            {
+                playerRigidbody.velocity = Vector2.right;
+            }
+        }   
+       else
         {
-            playerRigidbody.velocity = Vector2.right;
+            // User control 
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                playing = true;
+                playerRigidbody.velocity = Vector2.up * JUMP_AMOUNT;
+            }
         }
     }
 }
